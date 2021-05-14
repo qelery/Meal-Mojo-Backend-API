b065ecd77c3ce1eb62d7410b7648bb632f506ba0
Quincy Elery <quincyelery@gmail.com>
2021-05-14 10:28:58 -0500

    first commit

---
+++ src/main/java/com/qelery/mealmojoapibackend/MealMojoApiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.qelery.mealmojoapibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealMojoApiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealMojoApiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qelery/mealmojoapibackend/MealMojoApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qelery.mealmojoapibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealMojoApiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
