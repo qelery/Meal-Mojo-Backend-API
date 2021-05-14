c7f29a3b862f0ba94af1c217cde404bbfa5a6926
Quincy Elery <quincyelery@gmail.com>
2021-05-14 15:23:16 -0500

    security and jwt configurations and dependencies

--- src/main/java/com/qelery/mealmojo/api/service/UserService.java
+++ src/main/java/com/qelery/mealmojo/api/service/UserService.java
@@ -1,15 +1,22 @@
 package com.qelery.mealmojo.api.service;
 
+import com.qelery.mealmojo.api.model.User;
 import com.qelery.mealmojo.api.model.login.LoginRequest;
+import com.qelery.mealmojo.api.repository.UserRepository;
+import com.qelery.mealmojo.api.security.JwtUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
+    private final UserRepository userRepository;
     private final UserDetailsService userDetailsService;
+    private final JwtUtils jwtUtils;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
@@ -17,6 +24,7 @@ public class UserService {
     public UserService(UserRepository userRepository,
                        UserDetailsService userDetailsService,
                        PasswordEncoder passwordEncoder,
+                        JwtUtils jwtUtils,
                        AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.userDetailsService = userDetailsService;
@@ -35,6 +43,6 @@ public class UserService {
     }
 
     public User findByEmail(String email) {
-        if (userRepository.findBy)
+        return userRepository.findByEmail(email);
     }
 }
