13050409c11e86703ba952b9957043a696e85e54
Quincy Elery <quincyelery@gmail.com>
2021-05-15 09:44:20 -0500

    restuarant controller

--- src/main/java/com/qelery/mealmojo/api/controller/RestaurantController.java
+++ src/main/java/com/qelery/mealmojo/api/controller/RestaurantController.java
@@ -1,11 +1,11 @@
 package com.qelery.mealmojo.api.controller;
 
+import com.qelery.mealmojo.api.model.MenuItem;
 import com.qelery.mealmojo.api.model.RestaurantProfile;
 import com.qelery.mealmojo.api.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +20,65 @@ public class RestaurantController {
         this.restaurantService = restaurantService;
     }
 
+
+
+    // Restaurant endpoints
+
     @GetMapping("/restaurants")
     public List<RestaurantProfile> getRestaurants() {
         return restaurantService.getRestaurants();
+    }
+
+    @GetMapping("/restaurants/{restaurantId}")
+    public RestaurantProfile getRestaurant(@PathVariable Long restaurantId) {
+        return restaurantService.getRestaurant(restaurantId);
+    }
+
+    @PostMapping("/restaurants")
+    public RestaurantProfile createRestaurant(@RequestBody RestaurantProfile restaurantProfile) {
+        return restaurantService.createRestaurant(restaurantProfile);
+    }
+
+    @PutMapping("/restaurants")
+    public RestaurantProfile updateRestaurant(@RequestBody RestaurantProfile restaurantProfile) {
+        return restaurantService.updateRestaurant(restaurantProfile);
+    }
+
+    @DeleteMapping("/restaurants/{restaurantId}")
+    public ResponseEntity<String> deleteRestaurant(@PathVariable Long restaurantId) {
+        return restaurantService.deleteRestaurant(restaurantId);
+    }
+
+
+
+    // Menu Item endpoints
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems")
+    public List<MenuItem> getMenuItemsByRestaurant(@PathVariable Long restaurantId) {
+        return restaurantService.getMenuItemsByRestaurant(restaurantId);
+    }
+
+    @GetMapping("/restaurants/{restaurantId}/menuitems/{menuitemsId}")
+    public MenuItem getMenuItemByRestaurant(@PathVariable Long restaurantId,
+                                            @PathVariable Long menuitemsId) {
+        return restaurantService.getMenuItemByRestuarant(restaurantId, menuitemsId);
+    }
+
+    @PostMapping("/restaurants/{restaurantId}/menuitems/{menuitemsId}")
+    public MenuItem createMenuItem(@PathVariable Long restaurantId,
+                                   @PathVariable Long menuitemsId) {
+        return restaurantService.createMenuItem(restaurantId, menuitemsId);
+    }
+
+    @PutMapping("/restaurants/{restaurantId}/menuitems/{menuitemsId}")
+    public MenuItem updateMenuItem(@PathVariable Long restaurantId,
+                                   @PathVariable Long menuitemsId) {
+        return restaurantService.updateMenuItem(restaurantId, menuitemsId);
+    }
+
+    @PatchMapping("/restaurants/{restaurantId}/menuitems/{menuitemsId}")
+    public MenuItem changeMenuItemAvailability(@PathVariable Long restaurantId,
+                                            @PathVariable Long menuitemsId) {
+        return restaurantService.changeMenuItemAvailability(restaurantId, menuitemsId);
     }
 }
--- src/main/java/com/qelery/mealmojo/api/service/RestaurantService.java
+++ src/main/java/com/qelery/mealmojo/api/service/RestaurantService.java
@@ -1,6 +1,8 @@
 package com.qelery.mealmojo.api.service;
 
+import com.qelery.mealmojo.api.model.MenuItem;
 import com.qelery.mealmojo.api.model.RestaurantProfile;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,5 +13,32 @@ public class RestaurantService {
 
     public List<RestaurantProfile> getRestaurants() {
         return new ArrayList<>();
+    }
+
+    public RestaurantProfile getRestaurant(Long restaurantId) {
+    }
+
+    public RestaurantProfile createRestaurant(RestaurantProfile restaurantProfile) {
+    }
+
+    public RestaurantProfile updateRestaurant(RestaurantProfile restaurantProfile) {
+    }
+
+    public ResponseEntity<String> deleteRestaurant(Long restaurantId) {
+    }
+
+    public List<MenuItem> getMenuItemsByRestaurant(Long restaurantId) {
+    }
+
+    public MenuItem getMenuItemByRestuarant(Long restaurantId, Long menuitemsId) {
+    }
+
+    public MenuItem createMenuItem(Long restaurantId, Long menuitemsId) {
+    }
+
+    public MenuItem updateMenuItem(Long restaurantId, Long menuitemsId) {
+    }
+
+    public MenuItem changeMenuItemAvailability(Long restaurantId, Long menuitemsId) {
     }
 }
