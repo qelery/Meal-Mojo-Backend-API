8719649a4e5a0c0c2e2ca287db932d90e90fd68d
Quincy Elery <quincyelery@gmail.com>
2021-05-16 19:47:21 -0500

    method to calc distance between to geo coordinates

--- src/main/java/com/qelery/mealmojo/api/service/LocationService.java
+++ src/main/java/com/qelery/mealmojo/api/service/LocationService.java
@@ -13,7 +13,22 @@ public class LocationService {
         return new ArrayList<>();
     }
 
+    private double distanceApartInMiles(double latitudeA, double longitudeA, double latitudeB, double longitudeB) {
+        double radiusOfEarth = 6373.0; // km
 
+        latitudeA = Math.toRadians(latitudeA);
+        longitudeA = Math.toRadians(longitudeA);
+        latitudeB = Math.toRadians(latitudeB);
+        longitudeB = Math.toRadians(longitudeB);
+
+        double dLongitude = longitudeB - longitudeA;
+        double dLatitude = latitudeB - latitudeA;
+        double a = Math.pow(Math.sin(dLatitude / 2), 2) + Math.cos(latitudeA) * Math.cos(latitudeB) * Math.pow(Math.sin(dLongitude / 2), 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        double distance = radiusOfEarth * c; // km
+        return distance * 0.621371; // miles
+    }
 }
 
 
